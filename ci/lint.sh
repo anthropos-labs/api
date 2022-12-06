080fc0ba0e45f021f1b2648e470b585610791a36
Sienna Lloyd <sienna@linux.com>
2022-12-05 18:32:12 -0800

    testing new build process
    closes PLEIADES-42
    
    Signed-off-by: Sienna Lloyd <sienna@linux.com>

--- ci/lint.sh
+++ ci/lint.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-set -eux
+set -ux
 
 buf lint
 
 # kvstore
 cd databaseapi || exit
-buf breaking --against buf.build/anthropos-labs/kvstore
+buf breaking --against buf.build/anthropos-labs/kvstore --error-format junit > kvstore.xml
 cd .. || exit
 
 # errors
 cd errorapi || exit
-buf breaking --against buf.build/anthropos-labs/errors
+buf breaking --against buf.build/anthropos-labs/errors --error-format junit > errors.xml
 cd .. || exit
 
 # raft
 cd raftapi || exit
-buf breaking --against buf.build/anthropos-labs/raft
+buf breaking --against buf.build/anthropos-labs/raft --error-format junit > raft.xml
 cd .. || exit
+
+echo "##teamcity[importData type='junit' path='databaseapi/kvstore.xml']"
+echo "##teamcity[importData type='junit' path='errorapi/errors.xml']"
+echo "##teamcity[importData type='junit' path='raftapi/raft.xml']"
